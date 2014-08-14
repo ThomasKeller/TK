6cf25806a055f4a2a5146e19c7ad1db394e744a1
Thomas <tkdetrial@gmail.com>
2014-08-14 15:37:48 +0200

    Change to new QuartzWrapper

--- TK.ServiceCollector/src/KostalPlugin/KostalPlugin.cs
+++ TK.ServiceCollector/src/KostalPlugin/KostalPlugin.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Reflection;
-using Quartz;
-using Quartz.Impl;
+using TK.Logging;
+using TK.PluginManager;
+using TK.SimpleMessageQueue;
+using TK.WebPageParser;
 
 namespace TK.KostalPlugin
 {
-    public class KostalPlugin : PluginManager.QuartzPluginBase
+    public class KostalPlugin : SchedulePluginBase
     {
-        private const string c_PluginName = "KostalPlugin";
+        private static readonly ILogger _Logger = TK.Logging.LoggerFactory.CreateLoggerFor(typeof(PluginConfiguration));
 
-        public string Url { get; set; }
-        public string User { get; set; }
-        public string Password { get; set; }
+        private const string c_PluginName = @"KostalPlugin";
+        private const string c_QueuePath = @".\Private$\KostalValues2";
+
+        private SimpleMessageQueueWrapper<IDictionary<string, object>> _Queue;
+        public string Url { get; private set; }
+        public string User { get; private set; }
+        public string Password { get; private set; }
+        public string CronJobText { get; private set; }
 
         public override Dictionary<string, object> GetParameters()
         {
@@ -22,17 +27,23 @@ namespace TK.KostalPlugin
             defaultParameter.Add("URL", Url);
             defaultParameter.Add("User", User);
             defaultParameter.Add("Password", Password);
-            defaultParameter.Add("CronJobText", _CronJobText);
+            defaultParameter.Add("CronJobText", CronJobText);
             return defaultParameter;
         }
 
         public override void Initialize(Dictionary<string, string> initParams)
         {
-            Url = initParams.ContainsKey("URL") ? initParams["URL"] : "http://192.168.111.34";
+            Url = initParams.ContainsKey("URL") ? initParams["URL"] : @"http://192.168.111.34";
             User = initParams.ContainsKey("User") ? initParams["User"] : "pvserver";
             Password = initParams.ContainsKey("Password") ? initParams["Password"] : "geheim";
-            _CronJobText = initParams.ContainsKey("CronJob") ? initParams["CronJob"] : "0/60 * * * * ?";
-            InitializeJob();
+            CronJobText = initParams.ContainsKey("CronJob") ? initParams["CronJob"] : "0/60 * * * * ?";
+
+            var parameters = new Dictionary<string, object>();
+            parameters.Add("URL", Url);
+            parameters.Add("User", User);
+            parameters.Add("Password", Password);
+
+            _Scheduler.AddJob(CronJobText, Execute, parameters, false);
         }
 
         public override string PluginName()
@@ -45,19 +56,68 @@ namespace TK.KostalPlugin
             return Assembly.GetExecutingAssembly().GetName().Version.ToString();
         }
 
-        protected override void InitializeJob()
+        private void Execute(IDictionary<string, object> parameters)
         {
-            _JobDetail = JobBuilder.Create<KostalJob>()
-                                    .Build();
-            _JobDetail.JobDataMap.Add("URL", Url);
-            _JobDetail.JobDataMap.Add("User", User);
-            _JobDetail.JobDataMap.Add("Password", Password);
+            try
+            {
+                _Logger.DebugFormat("Execute KostalJob. Thread ID: {0}", System.Threading.Thread.CurrentThread.ManagedThreadId);
+                InitializeQueue();
+                var result = ParseKostalWebPage(parameters);
+                SendToQueue(result);
+                _Logger.DebugFormat("KostalJob Completed. Thread ID: {0}", System.Threading.Thread.CurrentThread.ManagedThreadId);
+            }
+            catch (Exception ex)
+            {
+                _Logger.FatalFormat("Fatal Error", ex.Message);
+            }
+        }
 
-            var trigger = TriggerBuilder.Create()
-                            .WithCronSchedule(_CronJobText) //"0 23 15,16,17 ? * *")
-                            .Build();
+        private static IDictionary<string, object> ParseKostalWebPage(IDictionary<string, object> parameters)
+        {
+            _Logger.DebugFormat("Read Value from Kostal - {0} ThreadId: {1}",
+                        System.DateTime.Now.ToString("r"),
+                        System.Threading.Thread.CurrentThread.ManagedThreadId);
 
-            _Scheduler.ScheduleJob(_JobDetail, trigger);
+            string webPage = WebPagerReader.Download(
+                                parameters["URL"] as string,
+                                parameters["User"] as string,
+                                parameters["Password"] as string);
+            if (!string.IsNullOrEmpty(webPage))
+            {
+                return KostalWebPageParser.Parse(webPage);
+            }
+            _Logger.Error("Web Page is emtpy");
+            return null;
+        }
+
+        private void InitializeQueue()
+        {
+            try
+            {
+                if (_Queue == null)
+                {
+                    _Logger.Debug("Create new Queue");
+                    _Queue = new SimpleMessageQueueWrapper<IDictionary<string, object>>() { MessageLabel = "Kostal" };
+                    _Queue.Initialize(c_QueuePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                _Logger.FatalFormat("Failed to initialize queue: {0}", ex.Message);
+                _Queue = null;
+            }
+        }
+
+        private void SendToQueue(IDictionary<string, object> result)
+        {
+            if (result != null)
+            {
+                _Queue.Send(result);
+            }
+            else
+            {
+                /// ToDO: store data somewhere
+            }
         }
     }
 }
